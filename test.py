902cad66f47823579af53d523ac6679d74a30a61
ibexes <j.cheang@warwick.ac.uk>
2017-02-09 16:07:46 +0000

    sockets
    connects to socket with funky functions!!

---
+++ test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import threading
+import socket
+
+#global declarations
+_running = 0
+
+#connect to data stream
+def connect_server():
+	host = "cs261.dcs.warwick.ac.uk"
+	host_port = 80
+	global _running 
+	_running = 1
+	netcat(host, host_port)
+
+#disconnects from data stream
+def disconnect_server():
+	global _running 
+	_running = 0
+
+
+#connects to host, port
+def netcat(host, port):
+	try:
+		#initialises socket
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	except socket.error:
+		print("Failed to create socket")
+
+	s.connect((host, int(port)))
+	print("Socket connected")
+	#filter out first line, first line is always [time,buyer,seller,price,size,currency,symbol,sector,bid,ask]
+	
+	global global_running
+	while (_running):
+		data = s.recv(4096);
+		if(len(data)>0):
+			print(data)
+
+
+#
+
+#main
+connect_server()
