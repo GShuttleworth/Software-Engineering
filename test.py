84636c3b816fdbba0485fd1ebd1d50d5b9a73921
ibexes <j.cheang@warwick.ac.uk>
2017-02-09 17:48:57 +0000

    Update test.py

--- test.py
+++ test.py
@@ -3,21 +3,24 @@ import threading
 import socket
 
 #global declarations
-_running = 0
+_running = 0 #overall program status
+_connected = 0 #connection to stream status
 
 #connect to data stream
-def connect_stream():
+def init_stream():
 	host = "cs261.dcs.warwick.ac.uk"
 	host_port = 80
-	global _running 
-	_running = 1
+	connect_stream()
 	netcat(host, host_port)
+
+def connect_stream():
+	global _connected 
+	_connected = 1
 
 #disconnects from data stream
 def disconnect_stream():
-	global _running 
-	_running = 0
-
+	global _connected 
+	_connected = 0
 
 #connects to host, port
 def netcat(host, port):
@@ -31,16 +34,38 @@ def netcat(host, port):
 	print("Socket connected")
 	#filter out first line, first line is always [time,buyer,seller,price,size,currency,symbol,sector,bid,ask]
 	
-	global global_running
-	while (_running):
-		data = s.recv(4096);
-		if(len(data)>0):
-			#todo
-			#each line begins with b' and ends with '
-			print(data)
+	global _running
+	global _connected
+	while(_running):
+		while (_connected):
+			data = s.recv(4096);
+			if(len(data)>0):
+				#todo
+				#each line begins with b' and ends with '
+				print(data) #stick data in a queue obv
 
 
-#
+#threading
+class myThread (threading.Thread):
+	def __init__(self, threadID):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+	def run(self):
+		print("Starting thread")
+		init_stream()
+
 
 #main
-connect_stream()
+_running = 1
+#create threads
+thread1 = myThread(1)
+
+#start threads
+thread1.start()
+
+while(_running):
+	var = input("Please enter something: ")
+	if(var=='d'):
+		disconnect_stream()
+	if(var=='c'):
+		connect_stream()
