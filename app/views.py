f4d123af4ff0fbfe34676b29215a917520e8ebd6
ibexes <j.cheang@warwick.ac.uk>
2017-02-10 18:02:14 +0000

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,7 +1,18 @@
 from flask import render_template
 from app import app
 
+from flask import request
+
 @app.route('/')
 @app.route('/index')
 def index():
-  return render_template('index.html')
+	pagename = "Home"
+	return render_template('index.html',pagename=pagename)
+
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+	pagename = "Home"
+	text = request.form['text']
+	print(text)
+	return render_template('index.html',pagename=pagename)
