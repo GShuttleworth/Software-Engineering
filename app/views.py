9c207f6bfe6c0cdfbf24a02f9090bfebfe7c30de
GShuttleworth <greg@gshuttleworth.co.uk>
2017-02-24 12:42:39 +0000

    Moved UI to Flask Template System
    Also added some extra charts and added basic routing

--- app/views.py
+++ app/views.py
@@ -6,8 +6,34 @@ from flask import request
 @app.route('/')
 @app.route('/index')
 def index():
-	pagename = "Home"
-	return render_template('index.html',pagename=pagename)
+	pagename = "Dashboard"
+	numberOfAnomalies = 500
+	numberOfTrades = 138282
+	totalTradeValue = 1500
+	anomalyID = 1
+	anomalyDate = "Timestamp"
+	anomalyType = "Pump and Dump"
+	return render_template('dashboard.html',pagename=pagename, numberOfAnomalies=numberOfAnomalies, numberOfTrades=numberOfTrades)
+
+@app.route('/anomaly')
+def anomaly():
+	pagename = "Anomaly Information"
+	anomalyType = "Pump and Dump"
+	anomalyStartTimestamp = "timestamp"
+	anomalyEndTimestamp = "timestamp"
+	certantyPercentage = "52"
+	time = "time"
+	buyer = "buyer"
+	seller = "seller"
+	price = "50"
+	size = "100"
+	currency = "GBP"
+	symbol = "DOLLA"
+	sector = "sector"
+	bid = "bid"
+	ask = "ask"
+	return render_template('anomaly.html',pagename=pagename)
+
 
 
 @app.route('/', methods=['POST'])
